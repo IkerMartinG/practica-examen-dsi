39e206215900d9d8978cfbd111c7a4783cc7c7a0
Iker Martin <alu0101556369@ull.edu.es>
2026-06-03 12:50:37 +0000

    segunda parte

--- src/models/booking.ts
+++ src/models/booking.ts
@@ -1,7 +1,71 @@
-import { Document, Schema, model } from "mongoose";
+import { Document, Schema, Types, model } from "mongoose";
 import { spaceCraftDocumentInterface } from "./spacecraft.js"
 import { passengerDocumentInterface } from "./passenger.js";
 
-interface bookingDocumentInterface extends Document {
-
+interface BookingPassenger {
+  passenger: passengerDocumentInterface,
+  seatType: "tourist" | "tourist-plus" | "vip"
 }
+
+interface bookingDocumentInterface extends Document {
+  spaceCraft: spaceCraftDocumentInterface,
+  passengers: BookingPassenger[],
+  departureDate: Date,
+  destination: "low-orbit" | "orbital-station" | "lunar-orbit" | "mars-orbit",
+  pricePerSeat: number,
+  totalAmount: number,
+  status: "confirmed" | "cancelled",
+  createdAt: Date
+}
+
+const BookingSchema = new Schema<bookingDocumentInterface> ({
+  spaceCraft: {
+    type: Types.ObjectId,
+    red: "SpaceCraft",
+    required: true
+  },
+  passengers: [
+    {
+      passenger: {
+        type: Types.ObjectId,
+        ref: "Passenger",
+        required: true
+      },
+      seatType: {
+        type: String,
+        required: true,
+        enum: ["tourist", "tourist-plus", "vip"]
+      }
+    }
+  ],
+  departureDate: {
+    type: Date,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true,
+    enum: ["low-orbit", "orbital-station", "lunar-orbit", "mars-orbit"]
+  },
+  pricePerSeat: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  totalAmount: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["confirmed", "cancelled"]
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+});
+
+export const Booking = model<bookingDocumentInterface>("Booking", BookingSchema);
---
+++ src/routes/spacecraft.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { SpaceCraft } from "../models/spacecraft.js";
+
+export const SpaceCraftRouter = express.Router();
+
+SpaceCraftRouter.get("", async (req, res) => {
+  
+})
